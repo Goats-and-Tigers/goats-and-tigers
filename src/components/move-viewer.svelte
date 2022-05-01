56891ae6b7ff44da39e922f5402b3a72921e1c5a
ashtyn3 <ashtynmorel@gmail.com>
2022-05-01 10:10:31 -0400

    fix(move-viewer): log doesn't go crazy on reload

--- src/components/move-viewer.svelte
+++ src/components/move-viewer.svelte
@@ -1,32 +1,34 @@
 <script lang="ts">
-    import { onMount } from "svelte";
+  import { onMount } from "svelte";
 
-    import { add_log, log } from "../log";
+  import { add_log, log } from "../log";
 
-    let el: HTMLDivElement;
-    let viewableLog = [];
-    onMount(() => {
-        let logString = localStorage.getItem("log");
-        let oldLog = JSON.parse(logString);
-        for (let i = 0; i < oldLog.length; i++) {
-            add_log(oldLog[i].replace(/\d\.\s+/, ""));
-        }
-    });
-    log.subscribe((l) => {
-        viewableLog = l;
-        if (el) {
-            el.scrollIntoView();
-        }
-    });
+  let el: HTMLDivElement;
+  let viewableLog = [];
+  let count = 0;
+
+  onMount(() => {
+    let logString = localStorage.getItem("log");
+    let oldLog = JSON.parse(logString);
+    for (let i = 0; i < oldLog.length; i++) {
+      add_log(oldLog[i]);
+    }
+  });
+  log.subscribe((l) => {
+    viewableLog = l;
+    if (el) {
+      el.scrollIntoView();
+    }
+  });
 </script>
 
 <div
-    class="ml-5 font-mono relative h-96 flex flex-col-reverse overflow-y-auto w-56"
+  class="ml-5 font-mono relative h-96 flex flex-col-reverse overflow-y-auto w-56"
 >
-    <div bind:this={el}>LOG</div>
-    <div>
-        {#each viewableLog as line}
-            <p>{line}</p>
-        {/each}
-    </div>
+  <div bind:this={el}>LOG</div>
+  <div>
+    {#each viewableLog as line, i}
+      <p>{i + 1}. {line}</p>
+    {/each}
+  </div>
 </div>
--- src/log.ts
+++ src/log.ts
@@ -1,16 +1,10 @@
 import { writable } from "svelte/store";
 
 export const log = writable<Array<string>>([]);
-export const logCount = writable<number>(0);
 
 export const add_log = (...args: Array<string>) => {
-  logCount.update((n) => n + 1);
-  let count;
-  logCount.subscribe((c) => {
-    count = c;
-  });
   log.update((e) => {
-    e.push(count + ". " + args.join(" "));
+    e.push(args.join(" "));
     return e;
   });
   log.subscribe((i) => {
