c131f4fd9df93692c4693d70a9cee3b3f844cc91
ashtyn3 <ashtynmorel@gmail.com>
2022-05-06 16:10:55 -0400

    fix(log): remove memory in local storage and log turn based

--- src/log.ts
+++ src/log.ts
@@ -1,13 +1,21 @@
 import { writable } from "svelte/store";
 
-export const log = writable<Array<string>>([]);
+export const log = writable<Array<Array<string>>>(
+  JSON.parse(localStorage.getItem("log")) || []
+);
+//let turn = [];
 
 export const add_log = (...args: Array<string>) => {
   log.update((e) => {
-    e.push(args.join(" "));
+    const last = e[e.length - 1];
+    if (e.length == 0 || last.length == 2) {
+      e.push([args.join(" ")]);
+    }
+    if (last.length != 2) {
+      last.push(args.join(" "));
+    }
+    localStorage.setItem("log", JSON.stringify(e));
     return e;
   });
-  log.subscribe((i) => {
-    localStorage.setItem("log", JSON.stringify(i));
-  });
+  return;
 };
