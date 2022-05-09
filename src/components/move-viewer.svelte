7a9dfbbe0de8019d97dc2fdca92fdbd812ea5dac
ashtyn3 <ashtynmorel@gmail.com>
2022-05-09 21:33:12 +0000

    chore: nevermind

--- src/components/move-viewer.svelte
+++ src/components/move-viewer.svelte
@@ -1,35 +1,33 @@
 <script lang="ts">
-  import { onMount } from "svelte";
+    import { onMount } from "svelte";
 
-  import { add_log, log } from "../log";
+    import { add_log, log } from "../log";
 
-  let el: HTMLDivElement;
-  let viewableLog = [];
+    let el: HTMLDivElement;
+    let viewableLog = [];
 
-  onMount(() => {
-    //let logString = localStorage.getItem("log");
-    //let oldLog = JSON.parse(logString);
-    //if (oldLog) {
-    //for (let i = 0; i < oldLog.length; i++) {
-    //viewableLog = [...viewableLog, oldLog[i]];
-    //}
-    //}
-  });
-  log.subscribe((l) => {
-    viewableLog = l;
-    if (el) {
-      el.scrollIntoView();
-    }
-  });
+    onMount(() => {
+        //let logString = localStorage.getItem("log");
+        //let oldLog = JSON.parse(logString);
+        //if (oldLog) {
+        //for (let i = 0; i < oldLog.length; i++) {
+        //viewableLog = [...viewableLog, oldLog[i]];
+        //}
+        //}
+    });
+    log.subscribe((l) => {
+        viewableLog = l;
+        if (el) {
+            el.scrollIntoView();
+        }
+    });
 </script>
 
-<div
-  class="ml-5 font-mono relative h-96 flex flex-col-reverse overflow-y-auto w-56"
->
-  <div bind:this={el}>LOG</div>
-  <div>
-    {#each viewableLog as line, i}
-      <p>{i + 1}. {line}</p>
-    {/each}
-  </div>
+<div class="ml-5 font-mono relative h-96 flex flex-col-reverse overflow-y-auto">
+    <div bind:this={el}>LOG</div>
+    <div>
+        {#each viewableLog as line, i}
+            <p>{i + 1}. {line}</p>
+        {/each}
+    </div>
 </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,30 @@
 module.exports = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    extend: {},
     colors: {
       lightGray: "#7A7A7A",
       lightGreen: "#F8FFEF",
     },
+  },
+  fontFamily: {
+    sans: ["neue-haas-grotesk-display"],
+    mono: ["IBM Plex Mono"],
+  },
+  screens: {
+    m2xl: { max: "1535px" },
+    // => @media (max-width: 1535px) { ... }
+
+    mxl: { max: "1279px" },
+    // => @media (max-width: 1279px) { ... }
+
+    mlg: { max: "1023px" },
+    // => @media (max-width: 1023px) { ... }
+
+    mmd: { max: "767px" },
+    // => @media (max-width: 767px) { ... }
+
+    msm: { max: "639px" },
+    // => @media (max-width: 639px) { ... }
   },
   plugins: [],
 };
