276c46773b9c451862830e4012636fac5e072970
ashtyn3 <ashtynmorel@gmail.com>
2022-05-14 13:30:41 +0000

    feat(multi): add rough multiplayer

--- src/pages/multi.svelte
+++ src/pages/multi.svelte
@@ -3,6 +3,11 @@
 
   import Board from "../Board.svelte";
   import { onMount } from "svelte";
+  import { db, user, username } from "../user";
+  import { v4 } from "uuid";
+  import Cookies from "js-cookie";
+  import { get } from "svelte/store";
+
   let rowNames = ["a", "b", "c", "d", "e", "f", "g", "h"];
   let names = {
     g: {
@@ -71,7 +76,7 @@
     },
   };
 
-  async function parse_fen(str: string) {
+  function parse_fen(str: string) {
     let board = {};
     const state = str.split(" ");
     localStorage.setItem("turn", state[1][0]);
@@ -97,6 +102,7 @@
               tile_type: "",
               tile_color: "",
               tile: "",
+              el: document.getElementById(id),
             };
           }
           colId += num;
@@ -106,6 +112,7 @@
             id,
             tile: get_tile(names[c].tile_color, names[c].tile_type),
             ...names[c],
+            el: document.getElementById(id),
           };
           colId++;
         }
@@ -113,11 +120,104 @@
     });
     return board;
   }
+
+  let input;
+  let game = false;
+  let game_id = "SAMPLE";
+  let buttons = true;
+  let join = false;
+  let menu = true;
+  let db_game;
+  let board_state = JSON.parse(localStorage.getItem("board"));
+  let board_fen = localStorage.getItem("fen_state");
+  let this_turn: any = "w";
   onMount(async () => {
-    const state = parse_fen("GHTBLMRS/8/8/8/8/2h5/g2m4/1htblmrs w");
-    const board = await state;
-    localStorage.setItem("board", JSON.stringify(board));
+    const params = new URLSearchParams(window.location.search);
+    if (!user.is && !Cookies.get("u")) {
+      window.location.href = "/login";
+    }
+    if (params.get("id")) {
+      game_id = params.get("id");
+      localStorage.setItem("game_id", game_id);
+      localStorage.setItem("you", "o");
+      db_game = db.get(game_id);
+      menu = false;
+    }
+    if (localStorage.getItem("game_id")) {
+      game_id = localStorage.getItem("game_id");
+      db_game = db.get(game_id);
+      menu = false;
+    }
+    if (!localStorage.getItem("fen_state")) {
+      localStorage.setItem("fen_state", "GHTBLMRS/8/8/8/8/8/8/ghtblmrs w");
+    }
+    db_game.map().once((e) => {
+      board_fen = e;
+      let board = parse_fen(e);
+      if (localStorage.getItem("turn") != localStorage.getItem("you")) {
+        this_turn = "";
+      } else {
+        this_turn = localStorage.getItem("turn");
+      }
+      localStorage.setItem("board", JSON.stringify(board));
+      board_state = board;
+    });
   });
 </script>
 
-<Board />
+{#if menu}
+  <div
+    class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 bg-[black] w-screen h-screen bg-opacity-90 flex justify-center items-center flex-col text-xl"
+  >
+    {#if buttons}
+      <div>
+        <button
+          class="m-2 bg-orange p-1"
+          on:click={async () => {
+            buttons = false;
+            game = true;
+            game_id = "game_" + v4().replace(/\-/g, "_");
+            let b = localStorage.getItem("fen_state");
+            localStorage.setItem("game_id", game_id);
+            localStorage.setItem("you", "w");
+            //@ts-ignore
+            db_game = db.get("game_" + game_id);
+            db_game.set(b);
+          }}>New game</button
+        >
+        <button
+          class="m-2 bg-orange p-1"
+          on:click={() => {
+            join = true;
+            buttons = false;
+          }}>Join game</button
+        >
+      </div>
+    {/if}
+    {#if game}
+      <p>Game Id</p>
+      <p class="text-base">(share with friends)</p>
+      <p class="text-orange">
+        http://localhost:3000/multi?id={game_id}
+      </p>
+    {/if}
+    {#if join}
+      <input
+        type="text"
+        bind:this={input}
+        class="bg-blue p-1"
+        placeholder="Game id"
+      />
+      <button class="m-2 bg-orange p-1">Join</button>
+    {/if}
+  </div>
+{/if}
+<p>
+  You: {localStorage.getItem("you")}
+</p>
+<p>
+  current turn: {board_fen.split(" ").length == 2
+    ? board_fen.split(" ")[1][0]
+    : ""}
+</p>
+<Board multi board={board_state} turn={this_turn} />
